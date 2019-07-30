194032b05fb1cf860c9aa9e4d235d98cbad0f47f
Dave Gabrielson <dave.gabrielson@gmail.com>
2019-07-30 11:48:57 -0500

    first commit

---
+++ datetimepicker/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from .views import JSTemplateView
+
+urlpatterns = [
+    url(r'^loader.js', JSTemplateView.as_view(), name='datetimepicker-loader')
+]
---
+++ sample/sample/views.py
@@ -0,0 +1,9 @@
+from django.views.generic.edit import FormView
+
+from .forms import SampleForm
+
+
+class SampleView(FormView):
+
+    form_class = SampleForm
+    template_name = 'sample.html'
